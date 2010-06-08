ebf240d3550bef7f86ca0c7fa6e399a96cb089c9
Wiktor Idzikowski <wiktor.idzikowski@gmail.com>
2010-06-08 13:02:45 +0200

    added docstrings in models.py, changed timezone to warsaw

--- tests/models.py
+++ tests/models.py
@@ -3,7 +3,14 @@ from django.db import models
 import datetime
 
 class Category(models.Model):
-    """ Model kategori: """
+    """Klasa kategori testów
+    
+        Opis pól:
+    
+        **name**
+      
+            Nazwa kategorii (100znaków)
+    """
     class Meta:
         verbose_name = "Kategoria"
         verbose_name_plural = "Kategorie"
@@ -14,28 +21,71 @@ class Category(models.Model):
         return u"%s" % self.name
 
 class User(models.Model):
+    """Klasa użytkownika wysyłającego testy - nauczyciela
+    
+    Opis pól:
+    
+        **login**
+      
+            Login, identyfikator nauczyciela (30 znaków)
+    
+        **password**
+            
+            Hasło (30 znaków)
+            
+        **full_name**
+        
+            Imię i nazwisko (100 znaków)
+    """
+        
     class Meta:
         verbose_name = "Użytkownika"
         verbose_name_plural = "Użytkownicy"
-    """ Model użytkownika wysyłającego testy:
-    1.login(login),
-    2.hasło(password)
-    3.imię i nazwisko """
+
     login = models.CharField(max_length=30, verbose_name="Nazwa użytkownika")
     password = models.CharField(max_length=30, verbose_name="Hasło")
     full_name = models.CharField(max_length=100, verbose_name="Imię i nazwisko")
-
+    
     def __unicode__(self):
         return self.full_name
 
 class Test(models.Model):
-    """ Model testu:
-    1.klucz obcy do użytkownika który wysłał testy
-    2.lokalizacje pliku z testem
-    3.opis testu """
+    """ Klasa testu, dane opisujące test zapisywane są w bazie danych, natomiast pliki z pytaniami i odpowiedziami na dysku.
+    
+        Opis pól:
+            
+            **user**
+            
+                Klucz obcy do użytkownika, który wysłał testy
+            
+            **category**
+            
+                Klucz obcy do kategorii testu
+                
+            **title**
+            
+                Tytuł testu (30 znaków)
+            
+            **instructions**
+            
+                Instrukcje do testu (256 znaków)
+                
+            **password**
+            
+                Hasło testu (30 znaków)
+                
+            **version**
+            
+                Wersja testu (liczba całkowita)
+                
+            **id_unq**
+            
+                Unikalne id testu (10 znaków)
+    """
     class Meta:
         verbose_name = "Test"
         verbose_name_plural = "Testy"
+        
     user = models.ForeignKey(User, verbose_name="Użytkownik")
     category = models.ForeignKey(Category, null=True, blank=True, verbose_name="Kategoria")
     title = models.CharField(max_length=30, verbose_name="Tytuł")
@@ -48,6 +98,30 @@ class Test(models.Model):
         return u"A: %s, T: %s, I: %s" %(self.user, self.title, self.instructions)
 
 class Result(models.Model):
+    """ Klasa wyników testu. Wynik punktowy zapisywany jest bazie danych. Lista odpowiedzi udzielonych przez ucznia zapisywana jest na dysku w formacie xml.
+    
+        Opis pól:
+            
+            **user_id_unq**
+            
+                Unikalne id użytkownika (30 znaków)
+            
+            **test_id_unq**
+            
+                Unikalne id testu (10 znaków)
+                
+            **points**
+            
+                Wynik z testu wyrażony w punktach (liczba zmiennoprzecinkowa)
+            
+            **points_percentage**
+            
+                Wynik z testu wyrażony w procentach (liczba zmiennoprzecinkowa)
+                
+            **ts_created**
+            
+                Stempel czasowy
+    """
     class Meta:
         verbose_name = "Wynik"
         verbose_name_plural = "Wyniki"
@@ -61,6 +135,26 @@ class Result(models.Model):
         return unicode("Uczeń:".decode('utf8')) + " %s, Test: %s, Wynik (punkty): %s, Wynik (procentowy): %s" % (self.user_id_unq, self.test_id_unq, self.points, self.points_percentage)
     
 class TestUser(models.Model):
+    """ Klasa opisująca obiekt rozwiązywanego testu. Zapisuje datę rozpoczęcia i odesłania wyników testu.
+    
+        Opis pól:
+            
+            **user_id_unq**
+            
+                Unikalne id użytkownika (30 znaków)
+            
+            **test_id_unq**
+            
+                Unikalne id testu (10 znaków)
+                               
+            **ts_created**
+            
+                Stempel czasowy
+                
+            **ts_returned**
+            
+                Stempel czasowy
+    """
     class Meta:
         verbose_name = "Użytkownicy rozwiązujący testy"
         verbose_name_plural = "Użytkownicy rozwiązujący testy"
